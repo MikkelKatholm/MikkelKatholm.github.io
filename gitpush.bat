a2259aaaedb6a7bbf5524adaec08b40ecfcf279c
LAPTOP-03D000GJ\mikke <202107199@post.au.dk>
2023-12-01 10:00:06 +0100

    test to make site work

--- gitpush.bat
+++ gitpush.bat
@@ -6,8 +6,15 @@ git status
 :: Add all changes to the staging area
 git add .
 
-:: Commit changes with a default message or customize it
-git commit -m "Update changes"
+:: Prompt the user for a commit message
+set /p commit_message="Enter your commit message: "
+
+:: Commit changes with the provided message or a default message if none is provided
+if "%commit_message%"=="" (
+  git commit -m "Update changes"
+) else (
+  git commit -m "%commit_message%"
+)
 
 :: Push changes to the remote repository (assuming the branch is 'main', change it if needed)
 git push origin main
