4a13327e19fdc8f91946d8dfa569d7ebe345935c
LAPTOP-03D000GJ\mikke <202107199@post.au.dk>
2024-01-06 21:21:35 +0100

    Update changes

--- cocktailbook/JS/navBar.js
+++ cocktailbook/JS/navBar.js
@@ -34,7 +34,7 @@ function createEventListener() {
             // Get the target from the "data-target" attribute
             const target = el.dataset.target;
             const $target = document.getElementById(target);
-
+            console.log(target)
             // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
             el.classList.toggle('is-active');
             $target.classList.toggle('is-active');
